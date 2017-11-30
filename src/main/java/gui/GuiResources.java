d49b25e613bc52357f1aec60c800a526c9ed2d82
Qwer Asder <peaser@ukr.net>
2017-11-30 09:56:55 +0200

    new branch for fixing problem with JTable

--- src/main/java/gui/GuiResources.java
+++ src/main/java/gui/GuiResources.java
@@ -4,21 +4,22 @@ import config.GlobalConfig;
 
 import java.util.Locale;
 import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
 
 public class GuiResources {
 
-    private static final String RESOURCES = "ContactResources";
+    private static final String RESOURCES = "src/main/resources/ContactResources";
     private static final String LANGUAGE = "language";
 
     private static PropertyResourceBundle components;
 
     public static void initComponentResources() {
         String lang = GlobalConfig.getProperty(LANGUAGE);
+        ResourceBundle bundle = ResourceBundle.getBundle("ContactResources", new Locale(lang));
         if (lang != null && !lang.trim().isEmpty()) {
+            components = (PropertyResourceBundle) bundle;
+        } else
             components = (PropertyResourceBundle) PropertyResourceBundle.getBundle(RESOURCES);
-        } else {
-            components = (PropertyResourceBundle) PropertyResourceBundle.getBundle(RESOURCES);
-        }
     }
 
     public static String getLabel(String formId, String componentId) {
